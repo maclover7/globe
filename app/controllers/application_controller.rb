ca710d7abddba5a655c49aeef0c1f1529a9e2878
maclover7 <me@jonathanmoss.me>
2015-10-10 10:10:56 -0400

    Add type to users, and more work on authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  %w(Student Teacher).each do |k|
+    define_method "current_#{ k.underscore }" do
+      current_user if current_user.is_a?(k.constantize)
+    end
+
+    define_method "authenticate_#{ k.underscore }!" do
+      |opts={}| send("current_#{ k.underscore }") || not_found
+    end
+
+    define_method "#{ k.underscore }_signed_in?" do
+      !send("current_#{ k.underscore }").nil?
+    end
+  end
 end
