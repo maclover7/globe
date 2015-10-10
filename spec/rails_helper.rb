1e02b343e14ad08dd3391f518f5a4e613996ddbb
maclover7 <me@jonathanmoss.me>
2015-10-10 09:47:14 -0400

    Add more testing helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'shoulda/context'
 require 'shoulda/matchers'
+require 'devise'
 require 'coveralls'
 Coveralls.wear!('rails')
 
@@ -14,4 +15,5 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
+  config.include Devise::TestHelpers, :type => :controller
 end
