b105143512be23b3272f33d57b9ee2c141b3eb86
maclover7 <me@jonathanmoss.me>
2015-10-10 16:54:06 -0400

    Add completed boolean to student assignments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151010190025) do
+ActiveRecord::Schema.define(version: 20151010205326) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer  "course_id"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20151010190025) do
     t.integer  "student_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.boolean  "completed"
   end
 
   create_table "users", force: :cascade do |t|
