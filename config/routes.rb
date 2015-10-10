11a6016b258a1a2ad598b0dc4de8e94680e44ad4
maclover7 <me@jonathanmoss.me>
2015-10-10 11:07:41 -0400

    lightly move around routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :courses
   root to: 'pages#home'
   get 'auth' => 'pages#auth'
+
+  resources :courses
 
   devise_for :users, skip: :registrations
   devise_for :students, :teachers, controllers: { registrations: "registrations" }, skip: :sessions
