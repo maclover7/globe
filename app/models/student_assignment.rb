207e2c3ca2a2250372e2c8889cd581096133698e
Jon Moss <me@jonathanmoss.me>
2015-10-24 19:05:01 -0400

    DRY up DashboardController
    - Add StudentAssignment.due_for_course helper
    - Add StudentAssignment.due_for_time helper

--- app/models/student_assignment.rb
+++ app/models/student_assignment.rb
@@ -1,4 +1,25 @@
 class StudentAssignment < ActiveRecord::Base
   belongs_to :assignment
   belongs_to :student
+
+  ## ASSIGNMENT HELPERS
+  def self.due_for_course(student, course_id)
+    student.student_assignments.where(completed: false).joins(:assignment).where("assignments.course_id = ?", course_id).all
+  end
+  
+  def self.due_for_time(student, time)
+    if time == 'overdue'
+      student.student_assignments.where(completed: false).joins(:assignment).where("assignments.due_date <= ?", Date.today).all
+    elsif time == 'today'
+      student.student_assignments.where(completed: false).joins(:assignment).where("DATE(assignments.due_date) = ?", Date.today).all
+    elsif time == 'tomorrow'
+      student.student_assignments.where(completed: false).joins(:assignment).where("DATE(assignments.due_date) = ?", Date.tomorrow).all
+    elsif time == 'this_week'
+      student.student_assignments.where(completed: false).joins(:assignment).where("DATE(assignments.due_date) BETWEEN ? AND ?", Date.today.beginning_of_week, Date.today.end_of_week).all
+    elsif time == 'next_2_weeks'
+      student.student_assignments.where(completed: false).joins(:assignment).where("DATE(assignments.due_date) BETWEEN ? AND ?", Date.today.beginning_of_week, Date.today.end_of_week + 7.days).all
+    elsif time == 'all'
+      student.student_assignments.where(completed: false).all
+    end
+  end
 end
--- spec/models/student_assignment_spec.rb
+++ spec/models/student_assignment_spec.rb
@@ -7,4 +7,83 @@ RSpec.describe StudentAssignment, type: :model do
   it 'has a valid factory' do
     expect(FactoryGirl.build(:student_assignment)).to be_valid
   end
+
+  describe '#due_for_course' do
+    it 'returns the correct student_assignment' do
+      @course = FactoryGirl.create(:course)
+      @student = FactoryGirl.create(:student)
+      @enrollment = FactoryGirl.create(:enrollment)
+      @assignment = FactoryGirl.create(:assignment)
+      @student_assignment = FactoryGirl.create(:student_assignment)
+      expect(StudentAssignment.due_for_course(@student, @course.id).reload).to eq([@student_assignment])
+    end
+  end
+
+  describe '#due_for_time' do
+    context 'time == overdue' do
+      it 'returns the correct student_assignment' do
+        @course = FactoryGirl.create(:course)
+        @student = FactoryGirl.create(:student)
+        @enrollment = FactoryGirl.create(:enrollment)
+        @assignment = FactoryGirl.create(:assignment, due_date: Date.yesterday)
+        @student_assignment = FactoryGirl.create(:student_assignment)
+        expect(StudentAssignment.due_for_time(@student, 'overdue').reload).to eq([@student_assignment])
+      end
+    end
+
+    context 'time == today' do
+      it 'returns the correct student_assignment' do
+        @course = FactoryGirl.create(:course)
+        @student = FactoryGirl.create(:student)
+        @enrollment = FactoryGirl.create(:enrollment)
+        @assignment = FactoryGirl.create(:assignment, due_date: Date.today)
+        @student_assignment = FactoryGirl.create(:student_assignment)
+        expect(StudentAssignment.due_for_time(@student, 'today').reload).to eq([@student_assignment])
+      end
+    end
+
+    context 'time == tomorrow' do
+      it 'returns the correct student_assignment' do
+        @course = FactoryGirl.create(:course)
+        @student = FactoryGirl.create(:student)
+        @enrollment = FactoryGirl.create(:enrollment)
+        @assignment = FactoryGirl.create(:assignment, due_date: Date.tomorrow)
+        @student_assignment = FactoryGirl.create(:student_assignment)
+        expect(StudentAssignment.due_for_time(@student, 'tomorrow').reload).to eq([@student_assignment])
+      end
+    end
+
+    context 'time == this_week' do
+      it 'returns the correct student_assignment' do
+        @course = FactoryGirl.create(:course)
+        @student = FactoryGirl.create(:student)
+        @enrollment = FactoryGirl.create(:enrollment)
+        @assignment = FactoryGirl.create(:assignment, due_date: Date.today.end_of_week)
+        @student_assignment = FactoryGirl.create(:student_assignment)
+        expect(StudentAssignment.due_for_time(@student, 'this_week').reload).to eq([@student_assignment])
+      end
+    end
+
+    context 'next_2_weeks' do
+      it 'returns the correct student_assignment' do
+        @course = FactoryGirl.create(:course)
+        @student = FactoryGirl.create(:student)
+        @enrollment = FactoryGirl.create(:enrollment)
+        @assignment = FactoryGirl.create(:assignment, due_date: Date.today.end_of_week + 7.days)
+        @student_assignment = FactoryGirl.create(:student_assignment)
+        expect(StudentAssignment.due_for_time(@student, 'next_2_weeks').reload).to eq([@student_assignment])
+      end
+    end
+
+    context 'time == all' do
+      it 'returns the correct student_assignment' do
+        @course = FactoryGirl.create(:course)
+        @student = FactoryGirl.create(:student)
+        @enrollment = FactoryGirl.create(:enrollment)
+        @assignment = FactoryGirl.create(:assignment)
+        @student_assignment = FactoryGirl.create(:student_assignment)
+        expect(StudentAssignment.due_for_time(@student, 'all').reload).to eq([@student_assignment])
+      end
+    end
+  end
 end
