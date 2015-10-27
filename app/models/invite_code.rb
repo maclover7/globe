676eb7c45961ad5951157c465a479ef8e2476711
Jon Moss <me@jonathanmoss.me>
2015-10-27 18:11:40 -0400

    Generate unique invite code

--- app/models/invite_code.rb
+++ app/models/invite_code.rb
@@ -1,4 +1,16 @@
 class InviteCode < ActiveRecord::Base
+  before_save :generate_course_code
   validates_presence_of :code
   validates_uniqueness_of :code
+
+  private
+
+  def generate_course_code
+    code = SecureRandom.hex(4)
+    if InviteCode.find_by(code: code) != nil
+      generate_course_code
+    else
+      self.code ||= code
+    end
+  end
 end
