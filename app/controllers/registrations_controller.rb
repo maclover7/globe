778d1152b84a6ced74d1a04852f39b6a9c3a2d26
Jon Moss <me@jonathanmoss.me>
2015-10-27 21:00:52 -0400

    Finish implementing invite codes and remove teacher signup link from /auth

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,31 @@
 class RegistrationsController < Devise::RegistrationsController
+  def create
+    if resource_name == :teacher
+      @invite_code = params['teacher']['invite_code']
+      if @invite_code.nil? or !InviteCode.where(code: @invite_code).any?
+        redirect_to root_path, notice: 'Unauthorized'
+      else
+        params['teacher'].delete('invite_code')
+        super
+      end
+    else
+      super
+    end
+  end
+
+  def new
+    if resource_name == :teacher
+      @invite_code = params[:invite_code]
+      if params[:invite_code].nil? or !InviteCode.where(code: @invite_code).any?
+        redirect_to root_path, notice: 'Unauthorized'
+      else
+        super
+      end
+    else
+      super
+    end
+  end
+
   def sign_up(resource_name, resource)
     sign_in(:user, resource)
   end
